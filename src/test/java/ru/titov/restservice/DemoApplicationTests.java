9a2ea627e3d1f6fcfbaa89466095645e1d69a228
Vladimir Titov <vtitov4@yandex.ru>
2021-01-14 23:30:05 +0300

    промежуточный коммит

--- src/test/java/ru/titov/restservice/DemoApplicationTests.java
+++ src/test/java/ru/titov/restservice/DemoApplicationTests.java
@@ -1,13 +1,27 @@
 package ru.titov.restservice;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.titov.restservice.service.CurrencyService;
+import ru.titov.restservice.service.GifService;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @MockBean
+    private CurrencyService currencyService;
 
+    @MockBean
+    private GifService gifService;
+
+    @Test
+    public void getUrlTest() {
+        String url = currencyService.getUrl();
+        Assert.assertTrue(url.contains("giphy.com/embed"));
+    }
 }
